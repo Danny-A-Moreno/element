6361bb4c969865dd6508995efc09a7b308480da5
Danny-A-Moreno <danny13moreno45@gmail.com>
2026-03-30 21:46:25 -0500

    Solucion Final: Variables de entorno y limpieza de sesion

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
-// Usamos la IP directa para evitar el error de DNS (getaddrinfo)
+// Prioridad a las variables de entorno de Render
 define('DB_HOST', getenv('DB_HOST') ?: '46.101.4.153');
+define('DB_PORT', getenv('DB_PORT') ?: '3306');
 define('DB_NAME', getenv('DB_NAME') ?: 'butqlct4sops6f3gimib');
 define('DB_USER', getenv('DB_USER') ?: 'unncd9ladcewvvjd');
 define('DB_PASS', getenv('DB_PASS') ?: 'lvKNKZytzSLnGyWDgFRi');
 
-// Rutas (Asegúrate de que BASE_URL sea vacío para Render)
 define('BASE_URL', ''); 
-define('ADMIN_URL', BASE_URL . '/admin');
-define('IMAGES_URL', BASE_URL . '/imagenes');
+define('ADMIN_URL', '/admin');
+define('IMAGES_URL', '/imagenes');
 ?>
