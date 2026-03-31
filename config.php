a59415bb3de1c1b0e935dcf981ce3b3437142c91
Danny-A-Moreno <danny13moreno45@gmail.com>
2026-03-30 20:37:43 -0500

    Mudar a Docker y conectar DB de Clever Cloud

--- config.php
+++ config.php
@@ -1,6 +1,12 @@
 <?php
-// Configuración de rutas del proyecto
-define('BASE_URL', '/element');
+// Datos de conexión de Clever Cloud
+define('DB_HOST', 'butqlct4sops6f3gimib-mysql.services.clever-cloud.com'); 
+define('DB_NAME', 'butqlct4sops6f3gimib');
+define('DB_USER', 'unncd9ladcewvvjd');
+define('DB_PASS', 'lvKNKZytzSLnGyWDgFRi');
+
+// Rutas para Render (Sin prefijo /element para que funcione en la raíz)
+define('BASE_URL', ''); 
 define('ADMIN_URL', BASE_URL . '/admin');
 define('IMAGES_URL', BASE_URL . '/imagenes');
 ?>
