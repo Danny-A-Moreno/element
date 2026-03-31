13c4edfccb522f86b778faf71d21db8edea7c170
Danny-A-Moreno <danny13moreno45@gmail.com>
2026-03-31 11:35:20 -0500

    fix register de email innecesario

--- register.php
+++ register.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
 require_once __DIR__ . '/BaseDatos.php';
-require_once __DIR__ . '/email_service.php';
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     header('Location: index.php');
