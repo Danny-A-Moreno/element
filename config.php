1e0854a22a737456be1baf4be937e7772e23e278
Danny-A-Moreno <danny13moreno45@gmail.com>
2026-03-30 21:08:28 -0500

    Cambio de Hostname por IP directa para saltar error DNS

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
-// Datos de Clever Cloud
-define('DB_HOST', 'butqlct4sops6f3gimib-mysql.services.clever-cloud.com'); 
-define('DB_PORT', '3306'); // El puerto que sale en tu captura
+// Usamos la IP directa para evitar el error de DNS (getaddrinfo)
+define('DB_HOST', '46.101.4.153'); 
+define('DB_PORT', '3306');
 define('DB_NAME', 'butqlct4sops6f3gimib');
 define('DB_USER', 'unncd9ladcewvvjd');
 define('DB_PASS', 'lvKNKZytzSLnGyWDgFRi');
 
-// Rutas
+// Rutas (Asegúrate de que BASE_URL sea vacío para Render)
 define('BASE_URL', ''); 
 define('ADMIN_URL', BASE_URL . '/admin');
 define('IMAGES_URL', BASE_URL . '/imagenes');
